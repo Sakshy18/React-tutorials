4a434c2dfd5cfb4fb474314e799d6cbec82844c0
Sakshi Thombre <84434398+Sakshy18@users.noreply.github.com>
2023-12-29 16:38:28 +0530

    added a random name generator function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App() {  
+  const randomName = () =>
+  {
+    const names=['sakshi','smriti','ayana','mihir'];
+    const i=Math.floor(Math.random() * 4);
+    return names[i];
+     
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hello {randomName()}!
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
